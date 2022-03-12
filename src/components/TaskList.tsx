2207ba44c410b9f8dd824635170bd9cca5bac61f
weldipaula <weldipaula@hotmail.com>
2022-03-11 22:36:39 -0300

    desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') {
+      return
+    }
+    const createTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(task => [...task,createTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskChecked = tasks.map(task => task.id === id ? {
+      id: task.id,
+      title: task.title,
+      isComplete: !task.isComplete 
+    } : task) 
+    setTasks(taskChecked)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskFiltered = tasks.filter(task => task.id !== id)
+    setTasks(taskFiltered)
   }
 
   return (
